bc90e6760b6d2849b0665a8f9f01e0fe4b9e8b11
simah88 <sangin.mah@gmail.com>
2022-05-13 21:15:02 -0700

    drivers

--- main_random.py
+++ main_random.py
@@ -8,31 +8,30 @@ import pandas as pd
 # "orate", "shiny" 
 answer_word_df = pd.read_csv('answers.txt', header= None)
 answer_word_list = answer_word_df[0].tolist()
-print( answer_word_list[0])
 attempt_count =[]
 no_answer_words=[]
-for word in answer_word_list:
-    helper = OccurrenceWordleStrategist()
+for word in answer_word_list[5:7]:
+    helper = RandomWordleStrategist()
     a = helper.give_initial_suggestions()
-    # print(a[0][0])
+    print(a)
     answer = WordleAgent(word)
     #answer = WordleAgent('watch')
     for i in range(6):
         
-        black_char, black_id, yellow_char, yellow_id, green_char, green_id = answer.attempt_word(a[0][0])
+        black_char, black_id, yellow_char, yellow_id, green_char, green_id = answer.attempt_word(a[0])
         print(black_char, black_id, yellow_char, yellow_id, green_char, green_id)
         a = helper.give_suggestions_from_obs(black_char, black_id, yellow_char, yellow_id, green_char, green_id)
         print(i, a, answer.word)    
         
         if green_id == [0,1,2,3,4]: #answer.word == a[0][0]
-            print('Win: within {0:2d} attempt(s)!'.format(i))
+            print('Win: with {0:2d} attempt(s)!'.format(i))
             attempt_count.append(i)
             break
         # if len(a) ==0:
         #     print('Not able to find solution from our word list')
         if i == 6:
             # print( 'answer word is', answer.word, ', last suggestion is ', a[0][0])
-            print('LOST! Fail to complete')
+            print('LOSE! Over 6 attempts')
             no_answer_words.append(word)
             break
         
